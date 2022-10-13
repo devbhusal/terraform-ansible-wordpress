5dc75fb008baaa8cc5112dfb3fb0bfd8973f3e73
dev <bhusal.dev1989@gmail.com>
2022-10-13 12:16:31 +1100

    terrform version fixes

--- main_script.tf
+++ main_script.tf
@@ -160,7 +160,7 @@ resource "aws_db_instance" "wordpressdb" {
   instance_class         = var.instance_class
   db_subnet_group_name   = aws_db_subnet_group.RDS_subnet_grp.id
   vpc_security_group_ids = ["${aws_security_group.RDS_allow_rule.id}"]
-  name                   = var.database_name
+  db_name                   = var.database_name
   username               = var.database_user
   password               = var.database_password
   skip_final_snapshot    = true
